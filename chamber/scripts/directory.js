1d7e43389af3824296ea6575de379f6b62803ee8
BaileyJensen <bjensen4@byupathway.edu>
2026-09-11 14:39:56 -0600

    test 1

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,48 @@
+const url = "https://baileyjensen.github.io/wdd231/chamber/data/prophets.json";
+
+const cards = document.querySelector('#cards');
+
+async function getCompanyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCompanies(data.companies);
+}
+
+
+
+const displayCompanies = (companies) => {
+
+    companies.forEach(company => {
+
+        let card = document.createElement("section");
+        let name = document.createElement("h2");
+        let logo = document.createElement("img");
+        let phone = document.createElement("p");
+        let address = document.createElement("p");
+        let website = document.createElement("p");
+        let membership = document.createElement("p");
+
+        name.textContent = `${company.name}`;
+        address.innerHTML = `<strong>Address: </strong> ${company.address}`;
+        phone.innerHTML = `<strong>Phone:</strong> ${company.phone}`;
+        website.innerHTML = `<strong>Website:</strong> ${company.website}`
+
+        logo.setAttribute("src", company.imageurl);
+        logo.setAttribute("alt", `Logo of ${company.name}`);
+        logo.setAttribute("loading", "lazy");
+        logo.setAttribute("width", "340");
+        logo.setAttribute("height", "440");
+
+        card.appendChild(name);
+        card.appendChild(phone);
+        card.appendChild(address);
+        card.appendChild(website);
+        card.appendChild(logo);
+
+        cards.appendChild(card);
+
+
+    });
+}
+
+getCompanyData();
